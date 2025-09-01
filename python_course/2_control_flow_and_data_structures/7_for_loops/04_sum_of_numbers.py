ad2cc09957638696729147b3b27b4f4b099720e9
krasi <krasimir.xristov@gmail.com>
2025-09-01 14:31:38 +0200

    lection 7 exercise 04

---
+++ python_course/2_control_flow_and_data_structures/7_for_loops/04_sum_of_numbers.py
@@ -0,0 +1,11 @@
+total_sum = 0
+start = 1
+stop = 101
+step = 1
+
+for number in range(start, stop, step):
+ total_sum = total_sum + number
+  
+
+
+print(f"The sum number from 1 to 100 is: {total_sum}")  
