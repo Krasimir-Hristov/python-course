53f9afe83516155164194fd338f01d6030cb86ba
krasi <krasimir.xristov@gmail.com>
2025-09-07 13:00:36 +0200

    Lection 9 exercise 02

---
+++ python_course/2_control_flow_and_data_structures/9_lists/02_access_list_elements.py
@@ -0,0 +1,4 @@
+days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+print(f"The first day is: {days_of_week[0]}")
+print(f"The fifth dai is: {days_of_week[4]}")
