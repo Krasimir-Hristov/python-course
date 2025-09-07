9a267a0a9a75d4dd43a9136b09c2dc938275cb53
krasi <krasimir.xristov@gmail.com>
2025-09-07 22:24:38 +0200

    Lection 10 exercise 06

---
+++ python_course/2_control_flow_and_data_structures/10_tuples/06_list_to_tuple_conversion.py
@@ -0,0 +1,5 @@
+itinerary_list = ['Rome', 'Berlin', 'Madrid']
+final_itinerary = tuple(itinerary_list)
+
+print(f"Mutable list: {itinerary_list}")
+print(f"Final immutable itinerary: {final_itinerary}")
