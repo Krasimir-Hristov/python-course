7dc52cfaf66edc80779f32dcbf9fca2e57f1414d
krasi <krasimir.xristov@gmail.com>
2025-09-07 22:17:59 +0200

    Lection 10 Exercise 05

---
+++ python_course/2_control_flow_and_data_structures/10_tuples/05_immutability_test.py
@@ -0,0 +1,6 @@
+my_tuple = (10, 20, 30)
+# Опит за промяна:
+# my_tuple[0] = 15
+# Този ред би предизвикал TypeError, защото кортежите са неизменяеми.
+print("Successfully demonstrated tuple immutability.")
+
