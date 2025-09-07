97b318791b17886d9fdd9ed5d3cc5ec73b708cdd
krasi <krasimir.xristov@gmail.com>
2025-09-07 12:14:23 +0200

    Lection 8 exercise 4

---
+++ python_course/2_control_flow_and_data_structures/8_while_loops/04_sum_until_negative.py
@@ -0,0 +1,18 @@
+
+
+sum_off_all_number = 0
+
+while True:
+  user_input = int(input("Enter a positive number (or a negative to exit):").strip())
+
+  if user_input < 0:
+    break
+
+  else:
+    sum_off_all_number += user_input
+    continue
+
+
+print(f"The sum of all positive numbers is: {sum_off_all_number}")
+
+
