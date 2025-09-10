11b5f1b0412309f86889c1dcdf5274ca88d95afb
krasi <krasimir.xristov@gmail.com>
2025-09-10 15:45:52 +0200

    Lection 11 exercise 05

---
+++ python_course/2_control_flow_and_data_structures/11_dictionaries/05_iterate_with_items.py
@@ -0,0 +1,15 @@
+capitals = {
+  "Bulgaria" : "Sofia",
+  "Germany" : "Berlin",
+  "France" : "Paris"
+}
+
+
+country_and_capital = capitals.items()
+
+for item in country_and_capital:
+ 
+   print(f"The capital of {item[0]} is {item[1]}")
+
+
+# Ima li nachin da stane tazi zadacha sus destruction kakto ima vuv JavaScript? Molq otgovori sled kato proverish testa, i mi kaji kak bi stanalo   
