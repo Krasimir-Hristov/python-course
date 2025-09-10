e77fd14d0123790dff9c1a61aa7138b59e97f370
krasi <krasimir.xristov@gmail.com>
2025-09-10 16:36:57 +0200

    Lection 12 exercise 05

---
+++ python_course/2_control_flow_and_data_structures/12_sets/05_set_intersection.py
@@ -0,0 +1,6 @@
+anna_hobbies = {"reading", "swimming", "coding"}
+boris_hobbies = {"coding", "traveling", "swimming"}
+
+common_hobbies = anna_hobbies.intersection(boris_hobbies)
+
+print(f"Common hobbies: {common_hobbies}")
