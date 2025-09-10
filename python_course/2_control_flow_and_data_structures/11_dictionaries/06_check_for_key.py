a24d6f3cba3b235c5798ef3c87249d7ea28855b7
krasi <krasimir.xristov@gmail.com>
2025-09-10 16:07:00 +0200

    correction exercise 06

--- python_course/2_control_flow_and_data_structures/11_dictionaries/06_check_for_key.py
+++ python_course/2_control_flow_and_data_structures/11_dictionaries/06_check_for_key.py
@@ -1,18 +1,7 @@
-user_premissions = {
-  "admin" : True,
-  "editor" : False
-}
-
+user_permissions = {"admin": True, "editor": False}
 check_role = input("Enter role to check:").lower().strip()
 
-roles = user_premissions.items()
-
-for role in roles:
- 
- if role[0] == check_role and role[1]:
-  print(f"Role {check_role} exist.")
-  break
- else:
-  print(f"Role {check_role} does not exist.")
-  break
-  
+if check_role in user_permissions: # Това е всичко!
+    print(f"Role '{check_role}' exists.")
+else:
+    print(f"Role '{check_role}' does not exist.")
