11aa878ebdec9497343453416cebb8c24b62eaaf
krasi <krasimir.xristov@gmail.com>
2025-09-30 20:08:21 +0200

    Add password checker script

---
+++ python_course/udemy_course/password_checker.py
@@ -0,0 +1,6 @@
+
+username = input('Please enter your username')
+
+password = input('Please enter your password')
+
+print(f'{username} your password { '*' * len(password)} is {len(password)} letter long')
